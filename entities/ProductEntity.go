1b0e91c9be395003cd1aba0143f78c5f7f91c45a
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-11 15:47:05 +0700

    fix: wrong data type for price column

--- entities/ProductEntity.go
+++ entities/ProductEntity.go
@@ -8,7 +8,7 @@ type ProductEntity struct {
 	Id          int       `gorm:"primaryKey;column:id" json:"id"`
 	Name        string    `gorm:"column:name" json:"name"`
 	Description string    `gorm:"column:description" json:"description"`
-	Price       int64     `gorm:"column:price" json:"price"`
+	Price       float64   `gorm:"column:price" json:"price"`
 	Stock       int       `gorm:"column:stock" json:"stock"`
 	CreatedAt   time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt   time.Time `gorm:"column:updated_at" json:"updatedAt"`
