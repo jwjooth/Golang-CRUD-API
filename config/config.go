1e28739ebdbd214cb277cb03e3b585fb7b9d3ff0
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-11 15:44:09 +0700

    feat: check environment variables for availability

--- config/config.go
+++ config/config.go
@@ -11,6 +11,13 @@ import (
 )
 
 func OpenDB() (*gorm.DB, error) {
+	requiredEnvs := []string{"DB_USER", "DB_PASSWORD", "DB_HOST", "DB_PORT", "DB_NAME"}
+	for _, env := range requiredEnvs {
+		if val := os.Getenv(env); val == "" {
+			return nil, fmt.Errorf("required environment variable %s is not set", env)
+		}
+	}
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
