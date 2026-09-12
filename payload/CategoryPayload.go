c5f90397cffb0990ab6f74c35e033c51ce29ff00
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-12 13:48:08 +0700

    feat: add category api

---
+++ payload/CategoryPayload.go
@@ -0,0 +1,14 @@
+package payload
+
+import "time"
+
+type CategoryRequest struct {
+	Name string `json:"name"`
+}
+
+type CategoryResponse struct {
+	ID uint `json:"id"`
+	Name string `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
