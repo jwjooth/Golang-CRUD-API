ee39e4e85f4a007ec802cdc7640282b48d3852bd
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-12 13:47:52 +0700

    feat: add book api

---
+++ payload/BookPayload.go
@@ -0,0 +1,32 @@
+package payload
+
+import "time"
+
+type BookRequest struct {
+	Title      string `json:"title" validate="required"`
+	CategoryID uint   `json:"category_id" validate="required"`
+	Author     string `json:"author" validate="required"`
+	Stock      int    `json:"stock" validate="required"`
+}
+
+type BookResponse struct {
+	ID         uint      `json:"id"`
+	Title      string    `json:"title"`
+	CategoryID uint      `json:"category_id"`
+	Author     string    `json:"author"`
+	Stock      int       `json:"stock"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
+
+type ListBookMeta struct {
+	Page      int `json:"page"`
+	PerPage   int `json:"per_page"`
+	Total     int `json:"total"`
+	TotalPage int `json:"total_page"`
+}
+
+type ListBookrResponse struct {
+	Data []BookResponse `json:"data"`
+	Meta ListBookMeta   `json:"meta"`
+}
