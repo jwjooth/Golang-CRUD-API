a8211c4e314c2f51c71c88d7f3319c070ad07593
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-13 20:56:19 +0700

    feat: add category repository and service layer

--- service/CategoryService.go
+++ service/CategoryService.go
@@ -1,1 +1,22 @@
 package service
+
+import (
+	"errors"
+	"golang-restful-api/payload"
+	"context"
+)
+
+type CategoryService interface {
+	GetAll(ctx context.Context) ([]payload.CategoryResponse, *helper.AppError)
+	GetById(ctx context.Context, id uint) (payload.CategoryResponse, *helper.AppError)
+	Create(ctx context.Context, request payload.CategoryRequest) (payload.CategoryResponse, *helper.AppError)
+	Update(ctx context.Context, request payload.CategoryRequest, id uint) (payload.CategoryResponse, *helper.AppError)
+	Delete(ctx context.Context, id uint) *helper.AppError
+}
+
+type CategoryServiceImpl struct {
+	repository *repository.CategoryRepository
+	validate *validator.Validate
+}
+
+
