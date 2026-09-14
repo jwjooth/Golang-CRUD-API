ff352fdbe9c2aa1f0d6d4db24be0021c76a91edf
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-14 11:37:49 +0700

    feat: continue to developing category crud api

--- repository/CategoryRepository.go
+++ repository/CategoryRepository.go
@@ -2,17 +2,18 @@ package repository
 
 import (
 	"context"
-	"golang-restful-api/entities"
-	"gorm.io/gorm"
 	"errors"
+	"golang-restful-api/entities"
+
+	"gorm.io/gorm"
 )
 
 type CategoryRepository interface {
 	GetAll(ctx context.Context) ([]entities.CategoryEntity, error)
 	GetById(ctx context.Context, id uint) (*entities.CategoryEntity, error)
 	Create(ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error)
-	Update(ctx context.Context, request *entities.Category)(*entities.CategoryEntity, error)
-	Delete(ctx context.Context, id uint) bool
+	Update(ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error)
+	Delete(ctx context.Context, id uint) error
 }
 
 type CategoryRepositoryImpl struct {
@@ -25,11 +26,11 @@ func NewCategoryRepositoryImpl(db *gorm.DB) *CategoryRepositoryImpl {
 	}
 }
 
-func (c *CategoryRepositoryImpl) GetAll (ctx context.Context) ([]entities.CategoryEntity, error){
+func (c *CategoryRepositoryImpl) GetAll(ctx context.Context) ([]entities.CategoryEntity, error) {
 	if err := c.db.WithContext(ctx).Model(&entities.CategoryEntity{}).Error; err != nil {
 		return nil, err
 	}
-	
+
 	var records []entities.CategoryEntity
 	query := c.db.WithContext(ctx).Order("id asc")
 	if err := query.Find(&records).Error; err != nil {
@@ -38,32 +39,31 @@ func (c *CategoryRepositoryImpl) GetAll (ctx context.Context) ([]entities.Catego
 	return records, nil
 }
 
-func (c *CategoryRepositoryImpl) GetById (ctx context.Context, id uint) (*entities.CategoryEntity, error){
+func (c *CategoryRepositoryImpl) GetById(ctx context.Context, id uint) (*entities.CategoryEntity, error) {
 	var category entities.CategoryEntity
-	if err := c.db.WithContext(ctx).First(category).Error; err != nil {
+	if err := c.db.WithContext(ctx).First(&category, id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errors.New("category not found")
 		}
 	}
-	return category, nil
+	return &category, nil
 }
 
-
-func (c *CategoryRepositoryImpl) Create (ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error){
+func (c *CategoryRepositoryImpl) Create(ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error) {
 	if err := c.db.WithContext(ctx).Create(request).Error; err != nil {
 		return nil, err
 	}
 	return request, nil
 }
 
-func (c *CategoryRepositoryImpl) Update (ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error) {
+func (c *CategoryRepositoryImpl) Update(ctx context.Context, request *entities.CategoryEntity) (*entities.CategoryEntity, error) {
 	if err := c.db.WithContext(ctx).Save(request).Error; err != nil {
 		return nil, err
 	}
 	return request, nil
 }
 
-func (c *CategoryRepositoryImpl) Delete (ctx context.Context, id uint) bool {
+func (c *CategoryRepositoryImpl) Delete(ctx context.Context, id uint) error {
 	result := c.db.WithContext(ctx).Delete(&entities.CategoryEntity{}, id)
 	if result.Error != nil {
 		return result.Error
