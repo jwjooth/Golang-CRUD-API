d4d61e0607711649d9f2d3629a5f2f61d87cb435
Jordan Ferdinand <jordan.ferdinand+IMSI@intern.indomobil.co.id>
2026-09-19 13:22:50 +0700

    feat: add constructor response payload

--- payload/CategoryPayload.go
+++ payload/CategoryPayload.go
@@ -1,6 +1,9 @@
 package payload
 
-import "time"
+import (
+	"golang-restful-api/entities"
+	"time"
+)
 
 type CategoryRequest struct {
 	Name string `json:"name"`
@@ -11,4 +14,21 @@ type CategoryResponse struct {
 	Name      string    `json:"name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewCategoryResponse(e entities.CategoryEntity) CategoryResponse {
+	return CategoryResponse{
+		ID:        e.ID,
+		Name:      e.Name,
+		CreatedAt: e.CreatedAt,
+		UpdatedAt: e.UpdatedAt,
+	}
+}
+
+func NewCategoryResponses(list []entities.CategoryEntity) []CategoryResponse {
+	out := make([]CategoryResponse, 0, len(list))
+	for _, e := range list {
+		out = append(out, NewCategoryResponse(e))
+	}
+	return out
 }
