7374f56eb1fb1f3d4f00d556d936333ed09392f2
jwjooth <185214226+jwjooth@users.noreply.github.com>
2026-09-23 20:16:11 +0700

    fix: wrong naming column

--- entities/ProductEntity.go
+++ entities/ProductEntity.go
@@ -16,8 +16,8 @@ type ProductEntity struct {
 	Rating      float64   `gorm:"column:rating;type:decimal(3,2)" json:"rating"`
 	ReviewCount int       `gorm:"column:reviewCount;default:0" json:"reviewCount"`
 	SKU         *string   `gorm:"column:sku;unique;size:50" json:"sku"`
-	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time `gorm:"column:updated_at;autoUpdateTime" json:"updated_at"`
+	CreatedAt   time.Time `gorm:"column:createdAt;autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time `gorm:"column:updatedAt;autoUpdateTime" json:"updated_at"`
 }
 
 func (*ProductEntity) TableName() string {
